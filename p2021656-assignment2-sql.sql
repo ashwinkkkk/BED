d226d353034a698891453a063d2d93cf7225ce24
ashwinkkkk <ashwin.kavidasan@gmail.com>
2023-01-02 23:03:22 +0800

    yes added

--- p2021656-assignment2-sql.sql
+++ p2021656-assignment2-sql.sql
@@ -29,7 +29,7 @@ CREATE TABLE `genre` (
   `genre` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   PRIMARY KEY (`genreid`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
